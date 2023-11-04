3a35065f31d6eaaffef57ba30c037106af745d41
Niranj <niranjcn024@gmail.com>
2023-11-04 18:53:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LoginSignup from './Componenets/Loginsignup-comp/LoginSignup';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
